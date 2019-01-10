002ff94168197a2a8efee64d92d857d41ae2a123
Dmitry Usenko <du.usenko.du@gmail.com>
2019-01-10 10:18:10 +0200

    Add project.

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Usenko Dmitry on 1/10/19.
+//  Copyright © 2019 Usenko Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
