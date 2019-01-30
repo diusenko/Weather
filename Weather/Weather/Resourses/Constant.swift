780d723ddad804a8cd60ce8487db8121b47ed901
Dmitry Usenko <du.usenko.du@gmail.com>
2019-01-30 17:37:46 +0200

    Constant.swift -> delete method weatherLink

--- Weather/Weather/Resourses/Constant.swift
+++ Weather/Weather/Resourses/Constant.swift
@@ -13,10 +13,4 @@ public struct Constant {
     static let weather = "Weather"
     static let capital = "Capital"
     static let celsius = UnitTemperature.celsius.symbol
-    
-    static let weatherLink: (String) -> String = { capital in
-        let query = "https://api.openweathermap.org/data/2.5/weather?q="
-        let unitsAndAppid = "&units=metric&APPID=93ed6d9a1885eeb796fe06b73dea3866"
-        return query + capital + unitsAndAppid
-    }
 }
