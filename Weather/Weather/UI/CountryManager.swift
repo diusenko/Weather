0decf8e6966939cc67bdda4808c549e6518a3b91
Dmitry Usenko <du.usenko.du@gmail.com>
2019-02-05 15:38:39 +0200

    CountryManager.swift -> Dependency injection. Remove completion.

--- Weather/Weather/UI/CountryManager.swift
+++ Weather/Weather/UI/CountryManager.swift
@@ -9,21 +9,24 @@
 import Foundation
 
 class CountryManager {
-    
-    private var model: [Country]?
-    
-    private let requestService = RequestService<[CountryJSON]>()
+    private let requestService: RequestService<[CountryJSON]>
     private let url = URL(string: "https://restcountries.eu/rest/v2/all")
     
-    public func fillModel(completion: @escaping F.Completion<[Country]?>) {
+    init(requestService: RequestService<[CountryJSON]> = RequestService<[CountryJSON]>()) {
+        self.requestService = requestService
+    }
+    
+    public func fillModel(countries: CountriesArrayModel) {
         if let url = self.url {
             self.requestService.loadData(url: url) { data, error in
                 if let data = data {
-                    self.model = data
-                        .map(Country.init)
+                    let countriesFromData = data
+                        .map {
+                            Country.init(name: $0.name, capitalName: $0.capital)
+                        }
                         .filter { $0.capital != "" }
+                    countries.append(countriesFromData)
                 }
-                completion(self.model)
             }
         }
     }
