50438c7793b2f903bbcfcd6fc14e19d88d14ccd9
Dmitry Usenko <du.usenko.du@gmail.com>
2019-02-13 16:54:13 +0200

    CountryNetworkService.swift -> Rebuild to NetworkTask

--- Weather/Weather/UI/CountryNetworkService.swift
+++ Weather/Weather/UI/CountryNetworkService.swift
@@ -9,19 +9,38 @@
 import Foundation
 
 class CountryNetworkService {
-    private let requestService: RequestService
+    
+    private let requestService: RequestServiceType
     private let url = URL(string: "https://restcountries.eu/rest/v2/all")
     
-    init(requestService: RequestService = .init()) {
+    init(requestService: RequestServiceType = RequestService(session: .default)) {
         self.requestService = requestService
     }
     
-    public func fillModel(model: CountriesArrayModel) {
-        if let url = self.url {
-            self.requestService.loadData(url: url) { data, error in
-                data.flatMap { try? JSONDecoder().decode([CountryJSON].self, from: $0) }
-                    .do { model.append(countries($0)) }
+//    func scheduledData(url: URL, completion: @escaping (Result<Data, RequestServiceError>) -> ()) -> NetworkTask {
+//        print("")
+//    }
+    
+    public func fillModel(model: CountriesArrayModel) -> NetworkTask {
+        
+        guard let url = self.url else {
+            let task = NetworkTask(sessionTask: .init())
+
+            defer {
+                task.cancel()
             }
+            
+            return task
+        }
+            
+        return self.requestService.scheduledData(url: url) { result in
+            result.analysis(
+                success: { data in
+                    let decoder = try? JSONDecoder().decode([CountryJSON].self, from: data)
+                    decoder.do { model.append(countries($0)) }
+            },
+                failure: { print($0) }
+            )
         }
     }
 }
