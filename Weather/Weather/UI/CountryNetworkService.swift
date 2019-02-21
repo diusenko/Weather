d1211421fc7d453bdc50eaf3a6db2939f3b02419
Dmitry Usenko <du.usenko.du@gmail.com>
2019-02-21 13:53:33 +0200

    CountryNetworkService.swift -> Rebuild constraint

--- Weather/Weather/UI/CountryNetworkService.swift
+++ Weather/Weather/UI/CountryNetworkService.swift
@@ -7,29 +7,28 @@
 //
 
 import Foundation
+import RealmSwift
 
-class CountryNetworkService {
+class CountryNetworkService<Type: DataBaseProvider>
+    where Type.Model == Country
+{
     
     private let requestService: RequestServiceType
     private let url = URL(string: "https://restcountries.eu/rest/v2/all")
-    private let dataBaseService: DataBaseService<CountryRLM>
+    private let dataBaseService: DataBaseService<Type>
     
     init(
         requestService: RequestServiceType = RequestService(session: .default),
-        dataBaseService: DataBaseService<CountryRLM>
+        dataBaseService: DataBaseService<Type>
     ) {
         self.requestService = requestService
         self.dataBaseService = dataBaseService
     }
     
-//    func scheduledData(url: URL, completion: @escaping (Result<Data, RequestServiceError>) -> ()) -> NetworkTask {
-//        print("")
-//    }
-    
     public func fillModel(model: CountriesArrayModel) -> NetworkTask {
         
         guard let url = self.url else {
-            let task = NetworkTask(sessionTask: .init())
+            let task = NetworkTask(sessionTask: nil)
 
             defer {
                 task.cancel()
@@ -42,14 +41,18 @@ class CountryNetworkService {
             result.analysis(
                 success: { data in
                     let decoder = try? JSONDecoder().decode([CountryJSON].self, from: data)
-                    //FixIT
+                    // FIXME: 111
                     decoder.do {
                         model.append(countries($0))
-                        $0.forEach {
-                            self.dataBaseService.dataBaseProvider.write(storage: RLMCountryJSON(json: $0))
+                        model.values.forEach {
+                            self.dataBaseService.value.write(model: $0)
                         }
                     }
-            },
+                    let read = self.dataBaseService.value.read()
+                    read.do { countries in
+                        model.append(countries)
+                    }
+                },
                 failure: { print($0) }
             )
         }
@@ -63,5 +66,5 @@ fileprivate let countries: ([CountryJSON]) -> [Country] = { json in
 }
 
 fileprivate let country: (CountryJSON) -> Country = {
-    Country(name: $0.name, capitalName: $0.capital)
+    Country(id: "", name: $0.name, capital: $0.capital)
 }
