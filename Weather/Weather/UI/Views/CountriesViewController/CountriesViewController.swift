3719bcced012f9caa60a78840bdb1864b698ae60
Dmitry Usenko <du.usenko.du@gmail.com>
2019-02-21 13:54:44 +0200

    CountriesViewController.swift -> Rebuild constraint

--- Weather/Weather/UI/Views/CountriesViewController/CountriesViewController.swift
+++ Weather/Weather/UI/Views/CountriesViewController/CountriesViewController.swift
@@ -8,21 +8,24 @@
 
 import UIKit
 
-class CountriesViewController: UIViewController, RootViewRepresentable {
+class CountriesViewController<DataBaseProviderType: DataBaseProvider>: UIViewController, RootViewRepresentable
+    where DataBaseProviderType.Model == Country
+{
     
     typealias RootView = CountriesView
     
     private let model: CountriesArrayModel
     
-    private let networkService: CountryNetworkService
+    private let networkService: CountryNetworkService<DataBaseProviderType>
     
     private let observableCountries = CancellableProperty()
     
     private let task = CancellableProperty()
     
-    init(model: CountriesArrayModel = .init(), networkService: CountryNetworkService) {
+    init(model: CountriesArrayModel = .init(), networkService: CountryNetworkService<DataBaseProviderType>) {
         self.networkService = networkService
         self.model = model
+        
         super.init(nibName: nil, bundle: nil)
         
         self.observableCountries.value = self.model.observer {
@@ -53,33 +56,7 @@ class CountriesViewController: UIViewController, RootViewRepresentable {
             self.rootView?.countriesTableView?.reloadData()
         }
     }
+    
+    
 }
 
-extension CountriesViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let countryData = self.model[indexPath.row]
-        
-        let weatherViewController = WeatherViewController(country: countryData)
-        
-        self.navigationController?.pushViewController(weatherViewController, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.model.values.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withCellClass: CountryTableViewCell.self, for: indexPath) {
-            let country = self.model.values[indexPath.row]
-            $0.completion = {
-                dispatchOnMain {
-                    tableView.reloadRow(at: indexPath, with: .automatic)
-                }
-            }
-            
-            $0.countryModel = country
-            //$0.fill(with: model.values[indexPath.row])
-        }
-    }
-}
