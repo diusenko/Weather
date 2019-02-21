a3dcedf3f5d09b87e76c6d329c6398354166506b
Dmitry Usenko <du.usenko.du@gmail.com>
2019-02-21 13:50:12 +0200

    AppDelegate.swift -> Refactoring

--- Weather/Weather/AppDelegate.swift
+++ Weather/Weather/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     {
         let window = UIWindow(frame: UIScreen.main.bounds)
         
-        let dataBaseService = DataBaseService(provider: CountryRLM())
+        let dataBaseService = DataBaseService(provider: DataRealm<RLMCountry>())
         
         let networkService = CountryNetworkService(dataBaseService: dataBaseService)
         
@@ -57,7 +57,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
